3211655e8d4304c365b23a181cd5928ee3af12ed
Mubashir Ali <21sw071@students.muet.edu.pk>
2025-01-31 22:54:08 +0500

    Working on Utils

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,37 @@
-import React , { useState , useEffect } from 'react'
-import { useDispatch } from 'react-redux';
-import authService from './appwrite/auth.service.js'
-import { login , logout } from './store/authSlice';
-import {Footer, Header} from "../src/components/index.js"
+import React, { useState, useEffect } from "react";
+import { useDispatch } from "react-redux";
+import authService from "./appwrite/auth.service.js";
+import { login, logout } from "./store/authSlice";
+import { Footer, Header } from "../src/components/index.js";
 
 function App() {
-  const [isLoading , setIsLoading] = useState(true);
+  const [isLoading, setIsLoading] = useState(true);
   const dispatch = useDispatch();
 
   useEffect(() => {
-      authService.getCurrentUser()
+    authService
+      .getCurrentUser()
       .then((userData) => {
-          if(userData) {
-            dispatch(login(userData))
-          } else{
-            dispatch(logout())
-          }
+        if (userData) {
+          dispatch(login(userData));
+        } else {
+          dispatch(logout());
+        }
       })
       .catch((error) => console.log(error))
       .finally(() => setIsLoading(false));
-  } , [])
+  }, []);
 
-  if(isLoading) {
+  if (isLoading) {
     return (
-      <div className='bg-gray-700 flex mx-auto justify-center items-center'>
+      <div className="bg-gray-700 flex mx-auto justify-center items-center">
         ...Loading the data
       </div>
-    )
+    );
   }
 
   return (
-    <div className=''>
+    <div className="">
       <Header />
       <h1>This is a blog Webiste</h1>
       <Footer />
